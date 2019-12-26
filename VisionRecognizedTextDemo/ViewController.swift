2c8eca030abcde9be3093567ad7ae73c3bd2ad35
Keishin CHOU <keishin.chou@gmail.com>
2019-12-26 13:04:33 +0900

    Initial Commit

---
+++ VisionRecognizedTextDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionRecognizedTextDemo
+//
+//  Created by Keishin CHOU on 2019/12/26.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
